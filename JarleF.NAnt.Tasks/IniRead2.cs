5d1be7d71a4ee52782d74c08192bedc4f24571b1
Jarle Friestad <jarle.friestad@gmail.com>
2010-10-26 10:32:18 +0200

    added IniRead2.cs task

---
+++ JarleF.NAnt.Tasks/IniRead2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JarleF.NAnt.Tasks.Utils;
+using NAnt.Core;
+using NAnt.Core.Attributes;
+using NAnt.Core.Util;
+
+namespace JarleF.NAnt.Tasks
+{
+    [TaskName("iniread2")]
+    public class IniReadTask : Task
+    {
+        // Fields
+        private string _iniFile;
+        private string _key;
+        private string _property;
+        private string _section;
+
+        public IniReadTask()
+        {
+            Default = null;
+        }
+
+        // Methods
+        protected override void ExecuteTask()
+        {
+            string str = string.Format("Retrieving {0}/{1} from file: {2}", Section, Key, FileName);
+            Log(Level.Verbose, LogPrefix + str);
+            try
+            {
+                Properties[Property] = new IniFile(FileName).GetString(Section, Key, Default);
+            }
+            catch (Exception exception)
+            {
+                throw new BuildException("Failed: " + str, Location, exception);
+            }
+        }
+
+        // Properties
+        [StringValidator(AllowEmpty = true), TaskAttribute("default", Required = true)]
+        public string Default { get; set; }
+
+        [StringValidator(AllowEmpty = false), TaskAttribute("filename", Required = true)]
+        public string FileName
+        {
+            get
+            {
+                return Project.GetFullPath(_iniFile);
+            }
+            set
+            {
+                _iniFile = StringUtils.ConvertEmptyToNull(value);
+            }
+        }
+
+        [TaskAttribute("key", Required = true), StringValidator(AllowEmpty = false)]
+        public string Key
+        {
+            get
+            {
+                return _key;
+            }
+            set
+            {
+                _key = StringUtils.ConvertEmptyToNull(value);
+            }
+        }
+
+        [StringValidator(AllowEmpty = false), TaskAttribute("property", Required = true)]
+        public string Property
+        {
+            get
+            {
+                return _property;
+            }
+            set
+            {
+                _property = StringUtils.ConvertEmptyToNull(value);
+            }
+        }
+
+        [TaskAttribute("section", Required = true), StringValidator(AllowEmpty = false)]
+        public string Section
+        {
+            get
+            {
+                return _section;
+            }
+            set
+            {
+                _section = StringUtils.ConvertEmptyToNull(value);
+            }
+        }
+    }
+
+
+}
---
+++ JarleF.NAnt.Tasks/Utils/IniFile.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace JarleF.NAnt.Tasks.Utils
+{
+    public class IniFile
+    {
+        // Methods
+        public IniFile(string fileName)
+        {
+            FileName = fileName;
+        }
+        
+
+        [DllImport("Kernel32.dll", EntryPoint = "GetPrivateProfileStringA", CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern int GetPrivateProfileString(string lpApplicationName, string lpKeyName, string lpDefault, StringBuilder lpReturnedString, int nSize, string lpFileName);
+        public string GetString(string section, string key, string @default)
+        {
+            var returnedString = new StringBuilder(1000);
+            if (GetPrivateProfileString(section, key, @default, returnedString, returnedString.Capacity, FileName) > 0)
+            {
+                return returnedString.ToString();
+            }
+            return string.Empty;
+        }
+
+        [DllImport("Kernel32.dll", EntryPoint = "WritePrivateProfileStringA", CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern int WritePrivateProfileString(string lpApplicationName, string lpKeyName, string lpString, string lpFileName);
+        public void WriteString(string section, string key, string text)
+        {
+            WritePrivateProfileString(section, key, text, FileName);
+        }
+
+        // Properties
+        public string FileName
+        {
+            get; private set;
+        }
+    }
+
+
+}
